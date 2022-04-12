f28b7599f597b04c81ce6d6c519c4f0f23c48567
ShahzaibPersonal <52831453+ShahzaibPersonal@users.noreply.github.com>
2022-04-12 21:43:00 +0500

    updated animations

--- js/app.js
+++ js/app.js
@@ -14,17 +14,12 @@ addBtn.addEventListener('click', () => {
     if (click == 0) {
 
         addBtn.classList.add("add_click");
-        addBtn.classList.add("absolute");
-        addBtn.classList.add("top-20");
-        addBtn.classList.add("mt-2");
+ 
         addTaskForm.classList.add("top-1/4")
         addTaskForm.classList.add("mt-5")
         addBtn.classList.add("object-scale-down");
 
-        addTaskForm.style.display = "block"
-        // addTaskFormBlur.style.display = "block"
-
-
+        addTaskForm.style.display = "inline"
         addBtn.classList.remove('close_click')
 
         addBtn.src = 'res/close.png';
@@ -36,12 +31,7 @@ addBtn.addEventListener('click', () => {
 
         addTaskForm.style.display = "none"
         addTaskFormBlur.style.display = "none"
-
-        addBtn.classList.remove("absolute");
-        addBtn.classList.remove("top-20");
-
         addBtn.classList.add('close_click')
-        addBtn.classList.remove('add_click')
 
         addBtn.src = 'res/add.png'
         click = 0;
